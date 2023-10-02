dd1cfceda01a9048658b46391e915abe4099c239
ArcticFox <kazimierz.luska@gmail.com>
2023-10-02 19:51:52 +0100

    Updating the build and nuget project configs

--- NukeBuild/Build.cs
+++ NukeBuild/Build.cs
@@ -6,39 +6,66 @@ using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Utilities.Collections;
+using YamlDotNet.Serialization;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
 
 class Build : NukeBuild
 {
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+    [Solution(GenerateProjects = true)]
+    readonly Solution Solution;
+
+    public static int Main () => Execute<Build>(x => x.Test);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
-    Target Clean => _ => _
+    Target Clean => run => run
         .Before(Restore)
         .Executes(() =>
         {
+            DotNetTasks.DotNetClean(settings =>
+                settings.SetProcessWorkingDirectory(RootDirectory));
         });
 
-    Target Restore => _ => _
+    Target Restore => run => run
         .Executes(() =>
         {
+            DotNetTasks.DotNetRestore(settings =>
+                settings.SetProcessWorkingDirectory(RootDirectory));
         });
 
-    Target Compile => _ => _
+    Target Test => run => run
         .DependsOn(Restore)
         .Executes(() =>
         {
+            DotNetTasks.DotNetTest(setting =>
+                setting.SetProcessWorkingDirectory(RootDirectory));
         });
+
+    Target Package_Core => run => run
+        .DependsOn(Test)
+        .Executes(() =>
+        {
+            DotNetTasks.DotNetPack(settings => settings
+                .SetProcessWorkingDirectory(Solution.src.SRGEnt_Core.Directory)
+                .SetConfiguration(Configuration.Release));
+        });
+
+    Target Package_Generator => run => run
+        .DependsOn(Test)
+        .Executes(() =>
+        {
+            DotNetTasks.DotNetPack(settings => settings
+                .SetProcessWorkingDirectory(Solution.src.SRGEnt_Generator.Directory)
+                .SetConfiguration(Configuration.Release));
+        });
+
+    Target PackForNuGet => run => run
+        .DependsOn(Package_Core, Package_Generator);
 
 }
