934fa6481b3fa064c3fea00bbefab6fe3e408b4d
ArcticFox <kazimierz.luska@gmail.com>
2023-10-08 11:57:44 +0100

    Parametrizing and commenting the code

--- Examples/MonoGame/SRGEnt/ECS_Definitions.cs
+++ Examples/MonoGame/SRGEnt/ECS_Definitions.cs
@@ -5,6 +5,11 @@ using SRGEnt.Attributes;
 // Using File scope namespace will prevent the generator from recognizing them as valid source
 namespace ECS_Mono_Example
 {
+    /// <summary>
+    /// The below definition is a template that the generator will use to creat Domain specific entities
+    /// for domains that use this type as their Entity.
+    /// Each of the properties in the interface will be available as components to be added on the output entity.
+    /// </summary>
     [EntityDefinition]
     public interface ISpriteEntity
     {
@@ -13,9 +18,17 @@ namespace ECS_Mono_Example
         Color Color { get; }
     }
 
+    /// <summary>
+    /// The below definition is a Domain declaration.
+    /// It will inform the generator that a domain SpriteBatchDomain
+    /// should be created with an Entity conforming to the ISpriteEntity API.
+    /// Additionally the domain has a Domain Scope "components" called ElapsedTime and CircleRadius which will be
+    /// accessible in all systems that operate on this domain. 
+    /// </summary>
     [DomainDefinition(typeof(ISpriteEntity))]
     public interface ISpriteBatchDomain
     {
         int ElapsedTime { get; }
+        int CircleRadius { get; }
     }
 }
--- Examples/MonoGame/SpinnerExampleGame.cs
+++ Examples/MonoGame/SpinnerExampleGame.cs
@@ -8,30 +8,34 @@ namespace MonoExample;
 public class SpinnerExampleGame : Game
 {
     private readonly SpriteBatchDomain _domain;
+    private readonly int _spritesCount;
     private RotatorSystem _rotator;
     private RendererSystem _renderer;
 
-    public SpinnerExampleGame()
+    public SpinnerExampleGame(int spritesCount)
     {
+        _spritesCount = spritesCount;
+        
         var graphics = new GraphicsDeviceManager(this);
         graphics.PreferredBackBufferWidth = 800;
         graphics.PreferredBackBufferHeight = 600;
+        
         Content.RootDirectory = "Content";
         IsMouseVisible = true;
 
-        _domain = new SpriteBatchDomain(5001);
+        _domain = new SpriteBatchDomain(_spritesCount + 1);
+        _domain.CircleRadius = 200; // Radius of the entity circle in pixels.
     }
 
     protected override void Initialize()
     {
-        const int spriteCount = 5000;
         const int spriteSize = 15;
-        for(var i = 0; i < spriteCount; i++)
+        for(var i = 0; i < _spritesCount; i++)
         {
             var spriteEntity = _domain.CreateEntity();
-            var color = new Color(255 * i / spriteCount, 255 * i / spriteCount, 255 * i / spriteCount);
+            var color = new Color(255 * i / _spritesCount, 255 * i / _spritesCount, 255 * i / _spritesCount);
             spriteEntity.Color = color;
-            spriteEntity.Angle = 360.0f / spriteCount * i;
+            spriteEntity.Angle = 360.0f / _spritesCount * i;
             spriteEntity.Size = new Point(spriteSize, spriteSize);
         }
 
@@ -42,6 +46,12 @@ public class SpinnerExampleGame : Game
     {
         _rotator = new RotatorSystem(_domain, .33f);
         _renderer = new RendererSystem(_domain, GraphicsDevice);
+    }
+
+    protected override void UnloadContent()
+    {
+        _renderer.DisposeTexture();
+        base.UnloadContent();
     }
 
     protected override void Update(GameTime gameTime)
